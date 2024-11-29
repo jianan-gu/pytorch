b5eac3f5753d5ebb95487b419fcd5ab65533058a
Wu, Chunyuan <chunyuan.wu@intel.com>
2024-11-29 01:40:56 -0500

    support other buffer to be symbol

--- torch/_inductor/kernel/flex_attention.py
+++ torch/_inductor/kernel/flex_attention.py
@@ -777,7 +777,8 @@ def flex_attention(
 
         buffer_list = placeholder_inps + list(score_mod_other_buffers) + mask_graph_placeholder_inps + list(mask_mod_other_buffers)
         for item in buffer_list:
-            fake_buffers.append(item.data.data)
+            if isinstance(item, TensorBox):
+                fake_buffers.append(item.data.data)
 
         (
             query,
@@ -837,7 +838,11 @@ def flex_attention(
             
             for prefix, buffers in [("score_others", score_mod_other_buffers), ("mask_others", mask_mod_other_buffers)]:
                 kernel_input_name_to_buffer.update({f"{prefix}_{i}": buf for i, buf in enumerate(buffers)})            
-            input_nodes += list(kernel_input_name_to_buffer.values())          
+            input_nodes += [
+                value 
+                for value in kernel_input_name_to_buffer.values() 
+                if not isinstance(value, sympy.Symbol)
+            ]         
 
         skip_mask_score = kernel_options.get("SKIP_MASK_SCORE", False)
         # Mark SPARSE_KV_BLOCK_SIZE & SPARSE_Q_BLOCK_SIZE as static shapes and add guards.
